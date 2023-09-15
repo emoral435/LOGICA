c24b08a2d3ae65fc0209ff8b27ce3c6ff2e8f8af
Eduardo Morales <emoral435@gmail.com>
2023-09-15 12:08:44 -0500

    Added React-Router-Dom as a dependency, added BrowserRouter as main router, created routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
 
   return (
     <>
-      Hello, we are Logica!
+      <nav>Hello, we are Logica!</nav> {/* NOTE this is where we can maybe put the navbar so that each page has the navbar that pops in*/}
+      <Routes>
+        <Route path="/" element={<h1>This is LOGICAS LANDING PAGE WOOOOOHOOOOOO</h1>} />
+        <Route path="/board" element={<h1>This is the boards section!</h1>} />
+        <Route path="/connect" element={<h1>This is where you connect with us and give us money muahahaha</h1>} />
+        <Route path="/events" element={<h1>This is where we will output the calendar / events that we have coming up</h1>} />
+      </Routes>
     </>
   )
 }
