859095e44323a159ec10eaf8d48e9618d8a7e0ab
Eduardo Morales <emoral435@gmail.com>
2023-09-19 19:53:17 -0500

    Basic NavBar look and functionality setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
-import { useAppSelector } from './features/hooks';
 import { presidentName, presidentEmail } from '../constants/Constants'
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
-  const { theme } = useAppSelector(state => state.theme);
-
   return (
     <>
       <NavBar /> {/* NOTE this is where we can maybe put the navbar so that each page has the navbar that pops in*/}
-      <h2 style={{height: '200vh'}}>We are currently using as the background theme {theme.primary.main}</h2>
+	  <h2 style={{height: '200vh'}}>adasd</h2>
       <Routes>
         <Route path="/" element={<h1>This is LOGICAS LANDING PAGE WOOOOOHOOOOOO</h1>} />
         <Route path="/board" element={<h1>This is the boards section!</h1>} />
--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,13 +1,38 @@
 import { useState, useEffect } from 'react';
-import './Navbar.css'; // Import your CSS file for styling
+import './NavBar.css'; // Import your CSS file for styling
+import { useNavigate } from 'react-router-dom';
+import LOGICA from '../../../public/logicaTree.svg'
+import { useAppSelector } from '../../features/hooks';
 
 const Navbar = () => {
+  const {theme} = useAppSelector(state => state.theme)	
+
   const [isVisible, setIsVisible] = useState(false);
+  const navigate = useNavigate()
+
+  const onClick = (e: React.ChangeEvent<any>) => {
+	const s: string = e.target.textContent
+
+	switch (s) {
+		case 'Connect':
+			navigate('/connect')
+			break;
+		case 'Events':
+			navigate('/connect')
+			break;
+		case 'Board':
+			navigate('/board')
+			break;
+		default:
+			navigate('/')
+		}
+
+	}
 
   useEffect(() => {
     // Function to handle scroll event
     const handleScroll = () => {
-      if (window.scrollY > 10) { // Adjust the threshold as needed
+      if (window.scrollY > 200) { // Adjust the threshold as needed
         setIsVisible(true);
       } else {
         setIsVisible(false);
@@ -25,11 +50,13 @@ const Navbar = () => {
 
   return (
     <div className={`navbar ${isVisible ? 'visible' : ''}`}>
-      {/* Your navbar content goes here */}
-      <a href="#">Home</a>
-      <a href="#">About</a>
-      <a href="#">Services</a>
-      <a href="#">Contact</a>
+      <a href="#" onClick={onClick} style={{color: theme.primary.contrastText}} className='navbar-logica'>
+		LOGICA
+		<img src={LOGICA} alt="logica logo" className='logica-tree' />
+	  </a>
+      <a href="#" onClick={onClick}>Connect</a>
+      <a href="#" onClick={onClick}>Board</a>
+      <a href="#" onClick={onClick}>Events</a>
     </div>
   );
 };
