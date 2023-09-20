f0759f1d69a698a1fb713231467a5e1503f2af29
Eduardo Morales <emoral435@gmail.com>
2023-09-19 19:56:24 -0500

    App.tsx cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
       <NavBar /> {/* NOTE this is where we can maybe put the navbar so that each page has the navbar that pops in*/}
-	  <h2 style={{height: '200vh'}}>adasd</h2>
       <Routes>
         <Route path="/" element={<h1>This is LOGICAS LANDING PAGE WOOOOOHOOOOOO</h1>} />
         <Route path="/board" element={<h1>This is the boards section!</h1>} />
