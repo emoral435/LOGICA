b56b70d557ada847aa61d03ee2672ca2285aeb49
Eduardo Morales <emoral435@gmail.com>
2023-09-22 16:41:03 -0500

    Condigured basic layout of top of landing page content, requires linking to other pages

--- frontend/src/Pages/Landing/Landing.tsx
+++ frontend/src/Pages/Landing/Landing.tsx
@@ -1,7 +1,22 @@
+import './Landing.css'
+import { useAppSelector } from "../../features/hooks"
+import LOGICA from '../../assets/logicaTree.svg'
 
 const Landing = () => {
+  const {theme} = useAppSelector(state => state.theme)
+
   return (
-    <div>Landing</div>
+    <section style={{backgroundColor: theme.primary.dark, color: theme.primary.contrastText}} className='logo-section'>
+      <div className='welcome'>
+        <div className='to'>welcome to</div>
+        <div className='is-logica'>LOGICA</div>
+        <div>latinx organization for growth in computing and academics</div>
+      </div>
+      {/* seperation of children on the landing page, will switch between the two on mobile */}
+      <div>
+        <img src={LOGICA} alt="logica tree logo" className='logo'/>
+      </div>
+    </section>
   )
 }
 
