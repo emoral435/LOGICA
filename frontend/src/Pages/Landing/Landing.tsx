b2fe7c0065dc5d4d1b33158554ab9a840497b806
Eduardo Morales <emoral435@gmail.com>
2023-09-27 22:52:23 -0500

    Switching between mac and windows

--- frontend/src/Pages/Landing/Landing.tsx
+++ frontend/src/Pages/Landing/Landing.tsx
@@ -1,22 +1,11 @@
-import './Landing.css'
-import { useAppSelector } from "../../features/hooks"
-import LOGICA from '../../assets/logicaTree.svg'
+import 'Landing.css'
+import LandingTop from '../../components/LandingTop/LandingTop'
 
 const Landing = () => {
-  const {theme} = useAppSelector(state => state.theme)
-
   return (
-    <section style={{backgroundColor: theme.primary.dark, color: theme.primary.contrastText}} className='logo-section'>
-      <div className='welcome'>
-        <div className='to'>welcome to</div>
-        <div className='is-logica'>LOGICA</div>
-        <div>latinx organization for growth in computing and academics</div>
-      </div>
-      {/* seperation of children on the landing page, will switch between the two on mobile */}
-      <div>
-        <img src={LOGICA} alt="logica tree logo" className='logo'/>
-      </div>
-    </section>
+    <main>
+        <LandingTop />
+    </main>
   )
 }
 
