7875c61e42d10f738f706991e2e1807d5ab0c151
Eduardo Morales <emoral435@gmail.com>
2023-10-05 09:24:12 -0500

    Added details and pictures to match members on member path

--- frontend/constants/Constants.tsx
+++ frontend/constants/Constants.tsx
@@ -1,22 +1,71 @@
-const presidentName: string = "Emily Mendoza"; // name of the current president
-const presidentEmail: string = "emend7@uic.edu"; // current email address of the president
-
+const presidentName: string = "Emily Victoria Mendoza"; // name of the current president
+const presidentEmail: string = "evmendoza37@gmail.com"; // current email address of the president
+import { Emily_M, Annie_Y, Valeria_G, Angel_Q, Darlene_M, Isabel_A, Eric_B, David_R, Erika_S, Eduardo_M, Brenda_L, Anthony_S, Lorena_C } from "../src/assets/BoardPics";
 
 const execBoard = [
 	{
-		name: "Emily Mendoza",
+		name: presidentName,
 		role: "President",
-		primaryEmail: "",
-		linkedIn: "",
+		primaryEmail: presidentEmail,
+		linkedIn: "https://www.linkedin.com/in/emily-v-mendoza37/",
 		github: "",
 		portfolio: "",
 		additionalLink: "",
-		funFact: "",
-		hobbies: ["Watching horror movies"],
+		funFact: "I do not have knuckles on my thumbs...",
+		hobbies: ["building computers", "playing video games", "any SWIM related sport", "going fast on my bike and surprising cars with how fast I am going on the street"],
+		pic: Emily_M
+	},
+	{
+		name: "Valeria Gogunska",
+		role: "Co-Vice President",
+		primaryEmail: "vgogun2@uic.edu",
+		linkedIn: "linkedin.com/in/valeriagogunska/",
+		github: "github.com/valegogu",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I speak 5 languages! (English, Spanish, Russian, Catalan, Chinese)",
+		hobbies: ["hiking", "learning lanuages", "traveling"],
+		pic: Valeria_G
+	},
+	{
+		name: "Annie You",
+		role: "Co-Vice President",
+		primaryEmail: "ayou4@uic.edu",
+		linkedIn: "https://www.linkedin.com/in/annie-you/",
+		github: "",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I was raised in Chetumal, Mexico and lived there for 10+ years!",
+		hobbies: ["doodling", "playing video games", "playing the ukelele", "watching anime / animated shows"],
+		pic: Annie_Y
+	},
+	{
+		name: "Angel Quinones",
+		role: "Treasurer and Web Administrator",
+		primaryEmail: "quinonesangel2000@gmail.com",
+		linkedIn: "linkedin.com/in/aquinones2000",
+		github: "https://github.com/AngelOfTheSkyGod",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I can vibrate my eyes rapidly!",
+		hobbies: ["making videogames!"],
+		pic: Angel_Q
 	},
 ]
 
 const boardMembers = [
+	{
+		name: "Darlene Marchan",
+		role: "Outreach Coordinator",
+		primaryEmail: "dmarch26@uic.edu",
+		linkedIn: "https://www.linkedin.com/in/darlene-marchan/",
+		github: "",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I am a fraternal twin!",
+		hobbies: ["traveling", "attending concerts", "scrapboarding", "reading"],
+		pic: Darlene_M
+	},
 	{
 		name: "Eduardo Morales",
 		role: "Jr Web Officer",
@@ -27,6 +76,91 @@ const boardMembers = [
 		additionalLink: "",
 		funFact: "I love to make jewelry and origami! Essentially, if I can craft it, I will enjoy it as a hobby.",
 		hobbies: ["watching comedy movies", "spending time outdoors", "going to the gym"],
+		pic: Eduardo_M
+	},
+	{
+		name: "Eric Bugarin",
+		role: "Club Communications",
+		primaryEmail: "ebugar4@uic.edu",
+		linkedIn: "https://www.linkedin.com/in/eric-bugarin/",
+		github: "",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I once went scuba diving in the ocean!",
+		hobbies: ["playing video games", "listening to music", "watching movies", "walking through trails", "watching YouTube"],
+		pic: Eric_B
+	},
+	{
+		name: "David Romero",
+		role: "Jr Communications Officer",
+		primaryEmail: "davidromero0315@gmail.com",
+		linkedIn: "https://www.linkedin.com/in/david-joel-romero/",
+		github: "",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I'm an only child!",
+		hobbies: ["learning languages! (human ones not coding LOL)"],
+		pic: David_R
+	},
+	{
+		name: "Isabel Arbaca",
+		role: "Jr Outreach/Volunteer Coordinator",
+		primaryEmail: "iabar2@uic.edu",
+		linkedIn: "https://www.linkedin.com/in/isabel-j-abarca/",
+		github: "https://github.com/isabel-abarca",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "Sticker bombing has become my personality",
+		hobbies: ["trying new foods & restaurants", "spending time hiking", "playing video games with my friends!"],
+		pic: Isabel_A
+	},
+	{
+		name: "Brenda Leyva",
+		role: "Social Media & Marketing Officer",
+		primaryEmail: "Bleyva3@uic.edu",
+		linkedIn: "https://www.linkedin.com/in/brenda-leyva01/",
+		github: "",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I have attended more than 20 concerts this year!",
+		hobbies: ["playing billboards", "playing video games", "attending and taking pictures at concerts!"],
+		pic: Brenda_L
+	},
+	{
+		name: "Erika Sy",
+		role: "Community Coordinator",
+		primaryEmail: "esy2@uic.edu",
+		linkedIn: "https://www.linkedin.com/in/erika-sy-0aba6025b/",
+		github: "",
+		portfolio: "",
+		additionalLink: "",
+		funFact: "I love pusheens!",
+		hobbies: ["playing video games"],
+		pic: Erika_S
+	},
+	{
+		name: "Anthony Sorto",
+		role: "Social Media Officer",
+		primaryEmail: "anthony.sorto97@gmail.com",
+		linkedIn: "linkedin.com/in/anthony-sorto",
+		github: "",
+		portfolio: "",
+		additionalLink: "I've ran the Chicago marathon 3 times!",
+		funFact: "I love pusheens!",
+		hobbies: ["martial arts", "working out!"],
+		pic: Anthony_S
+	},
+	{
+		name: "Lorena Castillejo",
+		role: "Social Media Officer Jr",
+		primaryEmail: "lcasti39@uic.edu",
+		linkedIn: "",
+		github: "",
+		portfolio: "",
+		additionalLink: "I've ran the Chicago marathon 3 times!",
+		funFact: "I love pusheens!",
+		hobbies: ["painting", "working out", "reading"],
+		pic: Lorena_C
 	},
 ]
 
---
+++ frontend/src/assets/BoardPics/index.ts
@@ -0,0 +1,29 @@
+import Emily_M from './Emily_M.png'
+import Annie_Y from './Annie_Y.png'
+import Valeria_G from './Valeria_G.png'
+import Angel_Q from './Angel_Q.png'
+import Darlene_M from './Darlene_M.png'
+import Isabel_A from './Isabel_A.png'
+import Eric_B from './Eric_B.png'
+import David_R from './David_R.png'
+import Erika_S from './Erika_S.png'
+import Eduardo_M from './Eduardo_M.png'
+import Brenda_L from './Brenda_L.png'
+import Anthony_S from './Anthony_S.png'
+import Lorena_C from './Lorena_C.png'
+
+export {
+	Emily_M,
+	Annie_Y,
+	Valeria_G,
+	Angel_Q,
+	Darlene_M,
+	Isabel_A,
+	Eric_B,
+	David_R,
+	Erika_S,
+	Eduardo_M,
+	Brenda_L,
+	Anthony_S,
+	Lorena_C
+}
